d4b9599ccfb3eafe589c3b546e56a1440cc021c9
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-05-25 11:54:11 +0200

    tests(docspec-python): Extend unit tests to cover more function signature parsing examples and class definitions

--- docspec-python/src/docspec_python/test/test_parser.py
+++ docspec-python/src/docspec_python/test/test_parser.py
@@ -20,11 +20,12 @@
 # IN THE SOFTWARE.
 
 from docspec import *
-from docspec_python import parse_python_module
+from docspec_python import parse_python_module, ParserOptions
 from functools import wraps
 from io import StringIO
 from json import dumps
 from textwrap import dedent
+from typing import List, Optional
 
 
 def unset_location(obj):
@@ -33,7 +34,7 @@ def unset_location(obj):
     unset_location(member)
 
 
-def docspec_test(**options):
+def docspec_test(module_name=None, parser_options=None):
   """
   Decorator for docspec unit tests.
   """
@@ -41,10 +42,13 @@ def docspec_test(**options):
   def decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-      options.setdefault('module_name', func.__name__.lstrip('test_'))
-      parsed_module = parse_python_module(StringIO(dedent(func.__doc__)), **options)
+      parsed_module = parse_python_module(
+        StringIO(dedent(func.__doc__)),
+        module_name=module_name or func.__name__.lstrip('test_'),
+        options=parser_options
+      )
       unset_location(parsed_module)
-      reference_module = Module(options['module_name'], None, None, func(*args, **kwargs))
+      reference_module = Module(parsed_module.name, None, None, func(*args, **kwargs))
       assert dumps(dump_module(reference_module), indent=2) == dumps(dump_module(parsed_module), indent=2)
     return wrapper
   return decorator
@@ -121,4 +125,202 @@ def test_funcdef_3():
         Decoration('db_session', '(sql_debug=True)'),
       ],
     )
+  ]
+
+
+@docspec_test()
+def test_funcdef_4():
+  """
+  def fun(project_name, project_type, port=8001):
+    pass
+  """
+
+  return [
+    Function(
+      name='fun',
+      location=None,
+      docstring=None,
+      modifiers=None,
+      args=[
+        Argument('project_name', Argument.Type.Positional, None, None, None),
+        Argument('project_type', Argument.Type.Positional, None, None, None),
+        Argument('port', Argument.Type.Positional, None, None, '8001'),
+      ],
+      return_type=None,
+      decorations=[],
+    )
+  ]
+
+
+@docspec_test(parser_options=ParserOptions(treat_singleline_comment_blocks_as_docstrings=True))
+def test_funcdef_5_single_stmt():
+  """
+  def func1(self): return self.foo
+
+  def func2(self):
+    # ABC
+    #   DEF
+    return self.foo
+
+  def func3(self):
+    ''' ABC
+      DEF '''
+    return self.foo
+
+  def func4(self):
+    '''
+    ABC
+      DEF
+    '''
+    return self.foo
+  """
+
+  def mkfunc(name: str, docstring: Optional[str]) -> Function:
+    return Function(
+      name=name,
+      location=None,
+      docstring=docstring,
+      modifiers=None,
+      args=[Argument('self', Argument.Type.Positional, None, None, None)],
+      return_type=None,
+      decorations=[],
+    )
+
+  return [
+    mkfunc('func1', None),
+    mkfunc('func2', 'ABC\nDEF'),
+    mkfunc('func3', 'ABC\nDEF'),
+    mkfunc('func4', 'ABC\n  DEF'),
+  ]
+
+
+@docspec_test()
+def test_funcdef_6_starred_args():
+  """
+  def func1(a, *, b, **c): pass
+
+  def func2(*args, **kwargs):
+    ''' Docstring goes here. '''
+
+  def func3(*, **kwargs):
+    ''' Docstring goes here. '''
+
+  def func4(abc, *,):
+    '''Docstring goes here'''
+
+  def func5(abc, *, kwonly):
+    '''Docstring goes here'''
+  """
+
+  def mkfunc(name: str, docstring: Optional[str], args: List[Argument]) -> Function:
+    return Function(
+      name=name,
+      location=None,
+      docstring=docstring,
+      modifiers=None,
+      args=args,
+      return_type=None,
+      decorations=[],
+    )
+
+  return [
+    mkfunc('func1', None, [
+      Argument('a', Argument.Type.Positional, None, None, None),
+      Argument('b', Argument.Type.KeywordOnly, None, None, None),
+      Argument('c', Argument.Type.KeywordRemainder, None, None, None),
+    ]),
+    mkfunc('func2', 'Docstring goes here.', [
+      Argument('args', Argument.Type.PositionalRemainder, None, None, None),
+      Argument('kwargs', Argument.Type.KeywordRemainder, None, None, None),
+    ]),
+    mkfunc('func3', 'Docstring goes here.', [
+      Argument('kwargs', Argument.Type.KeywordRemainder, None, None, None),
+    ]),
+    mkfunc('func4', 'Docstring goes here', [
+      Argument('abc', Argument.Type.Positional, None, None, None),
+    ]),
+    mkfunc('func5', 'Docstring goes here', [
+      Argument('abc', Argument.Type.Positional, None, None, None),
+      Argument('kwonly', Argument.Type.KeywordOnly, None, None, None),
+    ]),
+  ]
+
+
+@docspec_test()
+def test_classdef_1_exceptions():
+  """
+  class MyError1:
+    pass
+
+  class MyError2():
+    pass
+
+  class MyError3(RuntimeError):
+    pass
+
+  class MyError4(RuntimeError, metaclass=ABCMeta):
+    pass
+
+  class MyError5(metaclass=ABCMeta):
+    pass
+
+  class MyError6(RuntimeError):
+    __metaclass__ = ABCMeta
+  """
+
+  return [
+    Class(
+      name='MyError1',
+      location=None,
+      docstring=None,
+      metaclass=None,
+      bases=[],
+      decorations=None,
+      members=[]
+    ),
+    Class(
+      name='MyError2',
+      location=None,
+      docstring=None,
+      metaclass=None,
+      bases=[],
+      decorations=None,
+      members=[]
+    ),
+    Class(
+      name='MyError3',
+      location=None,
+      docstring=None,
+      metaclass=None,
+      bases=['RuntimeError'],
+      decorations=None,
+      members=[]
+    ),
+    Class(
+      name='MyError4',
+      location=None,
+      docstring=None,
+      metaclass='ABCMeta',
+      bases=['RuntimeError'],
+      decorations=None,
+      members=[]
+    ),
+    Class(
+      name='MyError5',
+      location=None,
+      docstring=None,
+      metaclass='ABCMeta',
+      bases=[],
+      decorations=None,
+      members=[]
+    ),
+    Class(
+      name='MyError6',
+      location=None,
+      docstring=None,
+      metaclass='ABCMeta',
+      bases=['RuntimeError'],
+      decorations=None,
+      members=[]
+    ),
   ]
