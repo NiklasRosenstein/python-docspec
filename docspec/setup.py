9a2cc39f569d428c1dc0af8f900e29826e5a1b9a
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-11 22:04:30 +0200

    regenerate setup files

--- docspec/setup.py
+++ docspec/setup.py
@@ -11,8 +11,7 @@ def _tempcopy(src, dst):
   import atexit, shutil
   if not os.path.isfile(dst):
     if not os.path.isfile(src):
-      print('warning: source file "{}" for destination "{}" does not exist'.format(src, dst))
-      return
+      raise RuntimeError('error: "{}" does not exist, and cannot copy it from "{}" either'.format(dst, src))
     shutil.copyfile(src, dst)
     atexit.register(lambda: os.remove(dst))
 
