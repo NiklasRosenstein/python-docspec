7d16342d9d7e2833b10494408edaeb3059dd14fd
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-12 02:02:57 +0200

    Address #15 and add ApiObject.parent (#17)
    * move unit tests in src/test
    
    * add very simple ReverseMap unit test
    
    * address #15, add ApiObject.parent
    
    * update action to trigger on develop only
    
    * regenerate setup.py
    
    * actually our main branch is called main
    
    * regenerate setup.py
    
    * remove test/__init__.py as that will cause an error when pytest tries to import the test modules for execution in one single process
    
    * fix test
    
    * add types-Deprecated to dev requirements, fix type hint

---
+++ docspec/src/test/test_reverse_map.py
@@ -0,0 +1,23 @@
+
+import docspec
+import pytest
+
+
+@pytest.fixture
+def module() -> docspec.Module:
+  module = docspec.Module('a', None, None, [
+    docspec.Class('foo', None, 'This is class foo.', None, None, None, [
+      docspec.Data('val', None, None, 'int', '42'),
+      docspec.Function('__init__', None, None, None, [
+        docspec.Argument('self', docspec.Argument.Type.Positional, None, None, None)
+      ], None, None),
+    ]),
+  ])
+  module.sync_hierarchy()
+  return module
+
+
+def test_reverse_map(module: docspec.Module) -> None:
+  rmap = docspec.ReverseMap([module])
+  assert rmap.get_parent(module) is None
+  assert rmap.get_parent(module.members[0]) is module
