877a069039a94d009d4b14dd1d9cf561473dfb0c
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-27 00:27:32 +0200

    fix up changelog

--- bin/generate-changelog.sh
+++ bin/generate-changelog.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 echo
+echo "__Table of Contents__"
+echo
+echo "* [Changelog](#changelog)"
+echo "* [docspec Changelog](#docspec-changelog)"
+echo "* [docspec_python Changelog](#docspec-python-changelog)"
+echo
+
+echo
+echo '# Changelog'
+echo
+shut changelog -a --markdown
+
+echo
 echo '# docspec Changelog'
 echo
 shut -C docspec changelog -a --markdown
