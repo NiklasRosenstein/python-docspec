1f2cf9751f453cff81b8cbfebdfc5970c138cce9
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-12-03 16:48:20 +0100

    Use UV instead of Slap

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.lint = pkgs.writeShellScriptBin "lint" ''
+          ${pkgs.ruff}/bin/ruff check .
+          ( cd docspec/ && ${pkgs.uv}/bin/uv run mypy . --check-untyped-defs )
+          ( cd docspec-python/ && ${pkgs.uv}/bin/uv run mypy . --check-untyped-defs )
+        '';
+
+        packages.test = pkgs.writeShellScriptBin "test" ''
+          ( cd docspec/ && ${pkgs.uv}/bin/uv run pytest . )
+          ( cd docspec-python/ && ${pkgs.uv}/bin/uv run pytest . )
+        '';
+
+        packages.docs = pkgs.writeShellScriptBin "docs" ''
+          ( cd docs/ && ${pkgs.uv}/bin/uv run novella --base-url docspec/ "$@" )
+        '';
+
+        formatter = pkgs.writeShellScriptBin "ruff" ''
+          ${pkgs.nixfmt-classic}/bin/nixfmt .
+          ${pkgs.ruff}/bin/ruff format .
+        '';
+
+        devShell = pkgs.mkShell { nativeBuildInputs = [ pkgs.uv ]; };
+      });
+}
